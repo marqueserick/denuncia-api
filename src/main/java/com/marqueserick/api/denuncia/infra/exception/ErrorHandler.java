e33eb3465e2f3d9a0e8e5b4ac55d21e7187dd524
Erick Marques <erickmarques43@gmail.com>
2023-02-24 12:10:58 -0300

    add NullPointException handler

--- src/main/java/com/marqueserick/api/denuncia/infra/exception/ErrorHandler.java
+++ src/main/java/com/marqueserick/api/denuncia/infra/exception/ErrorHandler.java
@@ -1,5 +1,7 @@
 package com.marqueserick.api.denuncia.infra.exception;
 
+import com.marqueserick.api.denuncia.dto.Resposta;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +11,16 @@ public class ErrorHandler {
 
     @ExceptionHandler(Erro.class)
     public ResponseEntity erroDenuncia(Erro e){
-        return new ResponseEntity(new ErroRetorno(e.getMessage(), e.getCode()), e.getHttpStatus());
+        return new ResponseEntity(new Resposta(e.getMessage(), e.getCode()), e.getHttpStatus());
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity erroNull(){
+        return new ResponseEntity(new Resposta("Campos não podem ser nulos","04"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity erro(){
+        return new ResponseEntity(new Resposta("Ocorreu um erro","00"), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
