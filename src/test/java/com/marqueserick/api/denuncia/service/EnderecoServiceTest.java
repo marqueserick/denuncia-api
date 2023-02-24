877927e4c7cc2bd25a0cd94d0c1a6b607e8e527d
Erick Marques <erickmarques43@gmail.com>
2023-02-24 11:22:10 -0300

    correct test

--- src/test/java/com/marqueserick/api/denuncia/service/EnderecoServiceTest.java
+++ src/test/java/com/marqueserick/api/denuncia/service/EnderecoServiceTest.java
@@ -6,9 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
